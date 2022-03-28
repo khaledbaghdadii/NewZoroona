a8eb7afe68c5f7c73e12997ce3b76ff1f16c1b0d
Khaled Baghdadi <khaledbaghdadi@Khaleds-MacBook-Pro.local>
2022-03-29 01:03:49 +0300

    set up prisma module

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     return 'I have signed up';
   }
