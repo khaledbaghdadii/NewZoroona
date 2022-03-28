8ccee6b3faab1aed261a816febcc852cc384de85
Khaled Baghdadi <khaledbaghdadi@Khaleds-MacBook-Pro.local>
2022-03-28 23:57:28 +0300

    inialize auth module

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signup() {
+    return "I'm signed up";
+  }
+  @Post('signin')
+  signin() {
+    return "I'm signed in";
+  }
+}
