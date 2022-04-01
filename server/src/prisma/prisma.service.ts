6c5a403fffc2fa05c112688bc48fb8b378197886
Khaled Baghdadi <khaledbaghdadi01@gmail.com>
2022-04-01 17:25:11 +0300

    add config module

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgres://utbbnmzcslnoui:6fe7aa9d99fa7a1870e5667705f2865a420536eb695313bc2a7f8dd91c62e227@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d3dev2isfsdfrb',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
