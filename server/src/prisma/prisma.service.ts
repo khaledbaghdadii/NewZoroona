ccf5194a081b3f4d8ead4475bcb47a4ea6a3f8e1
Khaled Baghdadi <khaledbaghdadi01@gmail.com>
2022-04-01 13:23:13 +0300

    add heroku remote postgres db

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5434/zoroona',
+          url: 'postgres://utbbnmzcslnoui:6fe7aa9d99fa7a1870e5667705f2865a420536eb695313bc2a7f8dd91c62e227@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d3dev2isfsdfrb',
         },
       },
     });
