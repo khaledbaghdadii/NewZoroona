09c722b4e4e456f6898aacd1d01503083983b34e
Carla Ghanem <carlaghanem_29@hotmail.com>
2022-04-03 20:47:16 +0300

    added place modules

---
+++ server/src/place/place.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, Req } from '@nestjs/common';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable({})
+export class PlaceService {
+  constructor(private prisma: PrismaService) {}
+  async getPlace(@Req() req) {
+    const place = await this.prisma.place;
+    return place;
+  }
+}
