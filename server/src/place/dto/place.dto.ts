0dbbaa068139312260159d8afd7e03956210b898
Carla Ghanem <carlaghanem_29@hotmail.com>
2022-04-06 00:39:19 +0300

    added crud for place

---
+++ server/src/place/dto/place.dto.ts
@@ -0,0 +1,90 @@
+import { Transform } from 'class-transformer';
+import {IsBoolean, IsEmail, IsInt, IsNotEmpty, IsString} from 'class-validator';
+
+export class UpdateDTO {
+  @IsEmail()
+  email: string;
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsString()
+  phoneNumber: string;
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+  @IsNotEmpty()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  placeId: number;
+  @IsString()
+  @IsNotEmpty()
+  district: string;
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+  @IsString()
+  location: string;
+  @IsString()
+  website: string;
+  @IsString()
+  @IsNotEmpty()
+  sector: string;
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+}
+
+export class AddDTO {
+  @IsEmail()
+  email: string;
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsString()
+  phoneNumber: string;
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+  @IsNotEmpty()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  placeId: number;
+  @IsString()
+  @IsNotEmpty()
+  district: string;
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+  @IsString()
+  location: string;
+  @IsString()
+  website: string;
+  @IsString()
+  @IsNotEmpty()
+  sector: string;
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+  @IsNotEmpty()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  categoryId: number;
+  @IsNotEmpty()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  orientationId: number;
+  @IsNotEmpty()
+  @IsInt()
+  @Transform(({ value }) => parseInt(value))
+  managerId: number;
+  @IsBoolean()
+  @IsNotEmpty()
+  @Transform(({ value }) => Boolean(value))
+  hasReservation: boolean;
+}
--- server/src/place/place.service.ts
+++ server/src/place/place.service.ts
@@ -1,13 +1,91 @@
-import { Injectable, Req } from '@nestjs/common';
+import {
+  Body,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Req,
+} from '@nestjs/common';
 
 import { PrismaService } from '../prisma/prisma.service';
+import { Place } from '@prisma/client';
+import { UpdateDTO, AddDTO } from '../place/dto';
 
 @Injectable({})
 export class PlaceService {
   constructor(private prisma: PrismaService) {}
-  async getPlace(@Req() req) {
-    // const place = await this.prisma.place;
-    const id = req.place.id;
-    return id;
+  async getPlace(placeId: number): Promise<Place | HttpException> {
+    const place = await this.prisma.place.findUnique({
+      where: {
+        id: placeId,
+      },
+    });
+    if (!place) {
+      return new HttpException('Place not found', HttpStatus.NOT_FOUND);
+    }
+    return place;
+  }
+  async deletePlace(placeId: number) {
+    try {
+      await this.prisma.place.delete({
+        where: {
+          id: placeId,
+        },
+      });
+    } catch (error) {
+      return new HttpException('Place not found', HttpStatus.NOT_FOUND);
+    }
+    return 'Deleted successfully';
+  }
+  async updatePlace(@Body() dto: UpdateDTO): Promise<Place | HttpException> {
+    const placeId = dto.placeId;
+    try {
+      const place = await this.prisma.place.update({
+        where: {
+          id: placeId,
+        },
+        data: {
+          email: dto.email,
+          name: dto.name,
+          phoneNumber: dto.phoneNumber,
+          city: dto.city,
+          district: dto.district,
+          address: dto.address,
+          location: dto.location,
+          website: dto.website,
+          sector: dto.sector,
+          description: dto.description,
+          image: dto.image,
+        },
+      });
+      return place;
+    } catch (err) {
+      return new HttpException('Place not found', HttpStatus.NOT_FOUND);
+    }
+  }
+  async addPlace(@Body() dto: AddDTO): Promise<Place | HttpException> {
+    try {
+      const place = await this.prisma.place.create({
+        data: {
+          email: dto.email,
+          name: dto.name,
+          phoneNumber: dto.phoneNumber,
+          city: dto.city,
+          district: dto.district,
+          address: dto.address,
+          location: dto.location,
+          website: dto.website,
+          sector: dto.sector,
+          description: dto.description,
+          image: dto.image,
+          categoryId: dto.categoryId,
+          managerId: dto.managerId,
+          hasReservation: dto.hasReservation,
+          orientationId: dto.orientationId,
+        },
+      });
+      return place;
+    } catch (err) {
+      return new HttpException('Place not found', HttpStatus.NOT_FOUND);
+    }
   }
 }
