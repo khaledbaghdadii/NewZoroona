f9c85a390736ce1da4f03f0de2ca4754716ac76e
Khaled Baghdadi <khaledbaghdadi01@gmail.com>
2022-04-05 12:03:53 +0300

    get specific user

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  ParseIntPipe,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
 import { UserGuard } from './guards/user.guards';
 import { UserService } from './user.service';
@@ -10,5 +17,9 @@ export class userController {
   @Get('me')
   getCurrentUser(@Req() req) {
     return this.userService.getCurrentUser(req);
+  }
+  @Get('/user')
+  getSpecificUser(@Query('userId', ParseIntPipe) userId: number) {
+    return this.userService.getSpecifcUser(userId);
   }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,6 +1,19 @@
-import { Injectable, Req } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  ParseIntPipe,
+  Query,
+  Req,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { IsInt } from 'class-validator';
 
 import { PrismaService } from '../prisma/prisma.service';
+class UserIdQuery {
+  @IsInt()
+  userId: number;
+}
 
 @Injectable({})
 export class UserService {
@@ -11,6 +24,17 @@ export class UserService {
         id: req.session.userId,
       },
     });
+    return user;
+  }
+  async getSpecifcUser(userId: number): Promise<User | HttpException> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
     return user;
   }
 }
