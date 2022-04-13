0bad5226dcfdb27aa5665fe6531c83432fbda302
Carla Ghanem <carlaghanem_29@hotmail.com>
2022-04-13 17:11:52 +0300

    place guards

--- server/src/place/place.controller.ts
+++ server/src/place/place.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Get, ParseIntPipe, Post, Query, Req} from '@nestjs/common';
+import {Body, Controller, Get, ParseIntPipe, Post, Query, Req, UseGuards} from '@nestjs/common';
 import { UpdateDTO, AddDTO } from './dto';
 import { PlaceService } from './place.service';
 import {Roles} from "../auth/decorators/roles.decorators";
+import {RolesGuard} from "../user/guards/role.guards";
 
 @Controller('places')
 export class PlaceController {
@@ -10,15 +11,22 @@ export class PlaceController {
   getPlace(@Query('placeId', ParseIntPipe) placeId: number) {
     return this.placeService.getPlace(placeId);
   }
+  @UseGuards(RolesGuard)
   @Post('delete')
+  @Roles('admin')
   deletePlace(@Body('placeId', ParseIntPipe) placeId: number) {
     return this.placeService.deletePlace(placeId);
   }
+  @UseGuards(RolesGuard)
   @Post('update')
+  @Roles('admin')
+  @Roles('manager')
   updatePlace(@Body() dto: UpdateDTO) {
     return this.placeService.updatePlace(dto);
   }
+  @UseGuards(RolesGuard)
   @Post('add')
+  @Roles('admin')
   addPlace(@Body() dto: AddDTO) {
     return this.placeService.addPlace(dto);
   }
