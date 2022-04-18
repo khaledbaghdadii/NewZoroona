aab41741ce221139b405dccf8bbb0c34e2638d3c
Carla Ghanem <carlaghanem_29@hotmail.com>
2022-04-18 18:25:35 +0300

    Add recommender module v1

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PlaceModule } from './place/place.module';
 import { ReviewModule } from './review/review.module';
 import { ReportModule } from './report/report.module';
+import {RecommenderModule} from "./recommender/recommender.module";
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ReportModule } from './report/report.module';
     PlaceModule,
     ReviewModule,
     ReportModule,
+    RecommenderModule
   ],
   controllers: [],
   providers: [],
