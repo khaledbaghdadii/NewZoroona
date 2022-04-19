e0313fdeb2a177680b9e1cc9ba4ee236739e0323
Carla Ghanem <carlaghanem_29@hotmail.com>
2022-04-19 23:42:51 +0300

    crud for reservation

---
+++ server/src/reservation/dto/reservation.dto.ts
@@ -0,0 +1,32 @@
+import { Transform } from 'class-transformer';
+import {IsDate, IsInt, IsNotEmpty, IsString} from 'class-validator';
+
+export class AddDTO {
+    @IsNotEmpty()
+    @IsInt()
+    @Transform(({ value }) => parseInt(value))
+    placeId: number;
+    @IsNotEmpty()
+    @IsInt()
+    @Transform(({ value }) => parseInt(value))
+    userId: number;
+    @IsNotEmpty()
+    @IsInt()
+    @Transform(({ value }) => parseInt(value))
+    numberofpeople: number;
+    @IsNotEmpty()
+    // @IsDate()
+    @Transform(({ value }) => new Date(value))
+    startdate: Date;
+    @IsNotEmpty()
+    // @IsDate()
+    @Transform(({ value }) => new Date(value))
+    enddate: Date;
+    @IsNotEmpty()
+    @Transform(({ value }) => parseInt(value))
+    cost: number;
+    @Transform(({ value }) => value.split(',').map(function(item) {
+        return parseInt(item, 10);
+    }))
+    packagesIds: number[];
+}
